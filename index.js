1fe980bf63a798cee3f9006d0b6521cecf21965c
Manish Kumar Sahu <msahu300@gmail.com>
2020-12-14 12:44:32 +0530

    added functionality with the API

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ var outputText = document.querySelector("#translatedText");
 var serverUrl = "https://api.funtranslations.com/translate/minion.json?";
 
 const translateMinion = (input) => {
-    return fetch(serverUrl + "text=" + input)
-    .then(response=>response.json())
-    .then(data=>console.log(data))
-    
-}
+  return fetch(serverUrl + "text=" + input)
+    .then((response) => response.json())
+    .then((data) => {
+      outputText.innerText = data.contents.translated;
+    });
+};
 
 handleBtn.addEventListener("click", () => {
   var userInput = inputText.value;
-  console.log(userInput);
   translateMinion(userInput);
 });
-
